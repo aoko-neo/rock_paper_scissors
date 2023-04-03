2be7eb37003ace29186d39f0459bca728424e19c
aoko-faith <97647953+aoko-neo@users.noreply.github.com>
2023-04-04 00:44:51 +0300

    evaluate choices given by player and computer

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // action to get a random computer choice
-
 function getComputerChoice() {
     let max = 2;
     let min = 0;
@@ -8,6 +7,19 @@ function getComputerChoice() {
     return guess[num];
 }
 
-//action to get player choice
-
 //action to compare player and computer choice
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === rock && computerSelection === scissors || playerSelection === paper && computerSelection === rock || playerSelection === scissors && computerSelection === paper) {
+        console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    } else if (playerSelection === scissors && computerSelection === rock || playerSelection === rock && computerSelection === paper || playerSelection === paper && computerSelection === scissors) {
+        console.log(`You lose ${computerSelection} beats ${playerSelection}`);
+    } else {
+        console.log('You draw')
+    }
+}
+
+let playerSelection = prompt('Please enter random choice').toLowerCase();
+
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
