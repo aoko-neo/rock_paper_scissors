94b96c9217a2f77855c1da9e6141b97b2cea56b3
Faith Aoko <97647953+aoko-neo@users.noreply.github.com>
2023-05-16 12:52:00 +0300

    add function game that plays five rounds of game

--- README.md
+++ README.md
@@ -1,10 +1,22 @@
 Rock, Paper, Scissors.
 A random game in javascript.
-//create a function to get a random computer choice
-//create a variable to get player choice
-   //prompt to ask a player to enter a guess
-   //make all entered variables lowercase
-//create a function  that take parameters from player and computer
-    //write code to evaluate values entered by computer and player
+create a function to get a random computer choice
+create a variable to get player choice
+   -prompt to ask a player to enter a guess
+   -make all entered variables lowercase
+
+     *playround
+create a function  that takes values from player and computer
+    -compare values entered by player and computer 
+    -tell the player if they win or lose
+   
+       *play five rounds
+create a function to play multiple rounds
+     -use playround as a parameter on this function
+     -loop through five rounds of game
+     -create a variable to hold results for each round
+     -determine overall winner after five rounds
+     -determine if player wins and increment value by 1 after every round
+
     
 
--- script.js
+++ script.js
@@ -7,14 +7,14 @@ function getComputerChoice() {
     return guess[num];
 }
 
-//action to compare player and computer choice
+//action to play one round
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+        return good;
     } else if (playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors') {
-        return `You lose ${computerSelection} beats ${playerSelection}`;
+        return bad;
     } else {
-        return 'You draw'
+        return draw;
     }
 }
 
@@ -22,4 +22,28 @@ let playerSelection = prompt('Please enter a random choice');
 
 let computerSelection = getComputerChoice();
 
+let good = 'You win';
+let bad = 'You lose';
+let draw = 'You draw';
+
+
+function game(play) {
+    let round = 5;
+    let playerWin = 0;
+    for (let i = 0; i < 5; i++){
+        if (i > 5) {
+            break;
+        } else {
+            play;
+        }
+        playerWin += 1;
+    }
+    return playerWin;
+}
+
+let play = playRound();
+
 console.log(playRound(playerSelection, computerSelection));
+console.log(game(play));
+
+
